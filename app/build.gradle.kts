0a6876a14ffe811b220ca97560561dbd1121240b
Hongwei Qin <122079993+shihuaidexianyu@users.noreply.github.com>
2026-04-19 19:41:57 +0800

    feat(icon): clean bank card icon; release 1.0.54

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.shihuaidexianyu.money"
         minSdk = 31
         targetSdk = 36
-        versionCode = 54
-        versionName = "1.0.53"
+        versionCode = 55
+        versionName = "1.0.54"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
