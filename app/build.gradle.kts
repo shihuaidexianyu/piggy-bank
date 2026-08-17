06aae0ad77eecedd1322c6e0d9bf4ba726e39507
Hongwei Qin <122079993+shihuaidexianyu@users.noreply.github.com>
2026-08-17 09:56:05 +0800

    chore: release 2.5.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-﻿import java.util.Properties
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
@@ -25,8 +25,8 @@ android {
         applicationId = "com.shihuaidexianyu.money"
         minSdk = 31
         targetSdk = 36
-        versionCode = 133
-        versionName = "2.5.24"
+        versionCode = 134
+        versionName = "2.5.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -134,6 +134,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
+
 
 
 
