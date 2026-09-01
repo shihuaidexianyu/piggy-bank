65dd3a3570370ff8a058f7ab16651062956b98ef
Hongwei Qin <122079993+shihuaidexianyu@users.noreply.github.com>
2026-09-01 23:36:51 +0800

    chore: release 2.5.34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.shihuaidexianyu.money"
         minSdk = 31
         targetSdk = 36
-        versionCode = 142
-        versionName = "2.5.33"
+        versionCode = 143
+        versionName = "2.5.34"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -134,6 +134,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
+
 
 
 
